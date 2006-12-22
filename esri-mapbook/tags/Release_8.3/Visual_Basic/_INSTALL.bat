1ca3c70d54234905af8416a489493607b980b6a5
maphew <maphew@6575c115-bc40-0410-b0c4-2562f1d3937b>
2006-12-22 22:32:43 +0000

    more pruning of unintended nesting and duplication

--- esri-mapbook/tags/Release_8.3/Visual_Basic/_INSTALL.bat
+++
@@ -1,6 +0,0 @@
-REM register component
-regsvr32 /s ".\DSMapBookPrj.dll"
-regsvr32 /s ".\DSMapBookUIPrj.dll"
-
-REM register components in appropriate component categories
-.\register_component_category.reg
