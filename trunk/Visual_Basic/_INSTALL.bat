e3f427f4fffd354a76681d1f0807febc56a2b3ce
maphew <maphew@6575c115-bc40-0410-b0c4-2562f1d3937b>
2007-09-07 16:53:48 +0000

    Added to pause to (un)install batch files so any error messages can be seen. _UNINSTALL.bat now deletes Mapbook registry keys

--- trunk/Visual_Basic/_INSTALL.bat
+++ trunk/Visual_Basic/_INSTALL.bat
@@ -1,6 +1,12 @@
-REM register component
+@echo off
+echo.
+
+echo.	Registering mapbook DLLs...
 regsvr32 /s ".\DSMapBookPrj.dll"
 regsvr32 /s ".\DSMapBookUIPrj.dll"
 
-REM register components in appropriate component categories
-.\register_component_category.reg
+echo.	Adding mapbook registry keys...
+regedit /s .\register_component_category.reg
+
+echo.
+pause
--- trunk/Visual_Basic/_UNINSTALL.bat
+++ trunk/Visual_Basic/_UNINSTALL.bat
@@ -1,3 +1,12 @@
-REM un-register component
+@echo off
+echo.
+
+echo.	De-registering mapbook DLLs...
 regsvr32 /s /u ".\DSMapBookUIPrj.dll"
 regsvr32 /s /u ".\DSMapBookPrj.dll"
+
+echo.	Removing mapbook registry keys...
+regedit /s .\de-register_component_category.reg
+
+echo.
+pause
