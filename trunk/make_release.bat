e13cdfdff92013179093497a6c705b640f4e9939
maphew <maphew@6575c115-bc40-0410-b0c4-2562f1d3937b>
2011-10-19 18:26:40 +0000

    prep for 2011-Oct-19 release, no functional changes, updated readme, make_release.bat and dependants.

--- trunk/make_release.bat
+++ trunk/make_release.bat
@@ -5,17 +5,28 @@ set outdir=D:\code\mapbook_extras\release_test\%1
 echo %outdir%
 if exist %outdir% goto :outExists
 
-mkdir %outdir%
-mkdir %outdir%\Visual_Basic
+call :Main
+Call :Extras
 
+start %outdir%
+goto :eof
+
+:: -------------------------------------------------------------------- 
 :Main
+  :: Copy single, named files from manifest to release folder
+  mkdir %outdir%\Visual_Basic
 	for /f "eol=¬ delims=" %%g in (manifest.txt) do (
-		copy "%%g" "%outdir%\%%g"
+		copy /v "%%g" "%outdir%\%%g"
 		)
-	start %outdir%
 	goto :eof
 
-
+:Extras
+  :: Recursively copy directories & contents in manifest to release folder
+	for /f "eol=¬ delims=" %%g in (manifest-extras.txt) do (
+		xcopy /s/i/v/q "%%g" "%outdir%\%%g"
+		)
+  goto :eof
+  
 :outExists
 	echo.
 	echo.	%outdir% already exists.
